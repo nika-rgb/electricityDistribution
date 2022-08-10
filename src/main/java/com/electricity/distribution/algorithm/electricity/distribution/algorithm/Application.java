72a077392a44e5be91cc1f05d3c1fbea378db589
nchek <nchek19@freeuni.edu.ge>
2022-08-10 20:28:01 +0400

    base configurations for spring boot project

---
+++ src/main/java/com/electricity/distribution/algorithm/electricity/distribution/algorithm/Application.java
@@ -0,0 +1,13 @@
+package com.electricity.distribution.algorithm.electricity.distribution.algorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
